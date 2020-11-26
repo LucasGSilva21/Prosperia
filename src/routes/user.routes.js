6686a7814f918572058978a840df2d5837c1c45d
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2020-11-26 19:05:15 -0300

    criando middleware para verificar token na rota

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 
 const UserController = require('../controllers/UserController');
+const authMiddleware = require('../middlewares/general/authMiddleware');
 
 const routes = new Router();
 
-routes.get('/', UserController.getAll);
-routes.get('/:userId', UserController.getOne);
+routes.get('/', authMiddleware, UserController.getAll);
+routes.get('/:userId', authMiddleware, UserController.getOne);
 routes.post('/', UserController.store);
-routes.delete('/:userId', UserController.delete);
+routes.delete('/:userId', authMiddleware, UserController.delete);
 
 module.exports = routes;
